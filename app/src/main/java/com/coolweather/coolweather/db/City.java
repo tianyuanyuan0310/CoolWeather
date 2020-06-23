e4eb6ac8983a64d81538cb62479ab7f19750e046
tianyuanyuan0310 <2292001848@qq.com>
2020-06-23 09:55:59 +0800

    2.0 添加city数据

---
+++ app/src/main/java/com/coolweather/coolweather/db/City.java
@@ -0,0 +1,14 @@
+package com.coolweather.coolweather.db;
+
+public class City {
+
+    private  int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
